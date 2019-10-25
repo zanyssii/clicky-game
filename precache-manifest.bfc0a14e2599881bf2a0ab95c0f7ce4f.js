ca0de5be0073a8886b97b36e667a4a4806eec09a
zanyssii <ataminnka@gmail.com>
2019-10-24 19:36:03 -0600

    Updates

---
+++ precache-manifest.bfc0a14e2599881bf2a0ab95c0f7ce4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6492a932a298a89d32b5706a69994fdd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "86eb1c5277101e7a5878",
+    "url": "/clicky-game/static/css/main.84d908c3.chunk.css"
+  },
+  {
+    "revision": "855d48f3ea4a913748d8",
+    "url": "/clicky-game/static/js/2.4936f835.chunk.js"
+  },
+  {
+    "revision": "86eb1c5277101e7a5878",
+    "url": "/clicky-game/static/js/main.bf729319.chunk.js"
+  },
+  {
+    "revision": "82793d2bc11076e28ef5",
+    "url": "/clicky-game/static/js/runtime~main.3d1d2641.js"
+  }
+]);
